91b27e28158bcea3dd6151465e585ec5fe19b2ad
daprabowo <dimasprabowo98@icloud.com>
2026-09-21 16:00:12 +0700

    feat(system): add more platform support

--- Sources/ChronoSystem/SystemTimeZone.swift
+++ Sources/ChronoSystem/SystemTimeZone.swift
@@ -1,9 +1,17 @@
 #if canImport(Darwin)
     import Darwin
+#elseif canImport(Bionic)
+    @preconcurrency import Bionic
 #elseif canImport(Glibc)
     @preconcurrency import Glibc
 #elseif canImport(Musl)
     @preconcurrency import Musl
+#elseif canImport(WinSDK)
+    import WinSDK
+#elseif os(WASI)
+    @preconcurrency import WASILibc
+#elseif os(Emscripten)
+    @preconcurrency import EmscriptenLibc
 #else
     #error("Unsupported platform: Standard C library not found.")
 #endif
